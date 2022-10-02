8b73de57a369ec8748329182bf72911c618c0184
Supun Lakmal Abesekara <supunlakmal61@gmail.com>
2022-10-02 10:37:07 +0000

    add key

--- checkout.ts
+++ checkout.ts
@@ -1,29 +1,26 @@
 import { loadStripe, Stripe } from "@stripe/stripe-js";
 
 export async function checkout({lineItems}:any){
+	let stripePromise: Promise<Stripe | null> | null = null;
 
-    console.log(process.env.NEXT_PUBLIC_API_KEY);
-    
-// 	let stripePromise: Promise<Stripe | null> | null = null;
+	const getStripe = () => {
+		if(!stripePromise) {
+			stripePromise = loadStripe(`${process.env.NEXT_PUBLIC_API_KEY}`)
+		}
+		return stripePromise
+	}
 
-// 	const getStripe = () => {
-// 		if(!stripePromise) {
-// 			stripePromise = loadStripe(process.env.NEXT_PUBLIC_API_KEY)
-// 		}
-// 		return stripePromise
-// 	}
+	const stripe = await getStripe()
 
-// 	const stripe = await getStripe()
-
-//     if(stripe){
+    if(stripe){
 
     
 
-// 	await stripe.redirectToCheckout({
-// 		mode: 'payment',
-// 		lineItems,
-// 		successUrl: `${window.location.origin}?session_id={CHECKOUT_SESSION_ID}`,
-// 		cancelUrl: window.location.origin
-// 	})
-// }
+	await stripe.redirectToCheckout({
+		mode: 'payment',
+		lineItems,
+		successUrl: `${window.location.origin}?session_id={CHECKOUT_SESSION_ID}`,
+		cancelUrl: window.location.origin
+	})
+}
 }
