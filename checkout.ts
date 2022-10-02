042d9dbbaf042b6d1e6e40188244aa2e9814b3c8
Supun Lakmal Abesekara <supunlakmal61@gmail.com>
2022-10-02 10:15:29 +0000

    update code

---
+++ checkout.ts
@@ -0,0 +1,26 @@
+import { loadStripe, Stripe } from "@stripe/stripe-js";
+
+export async function checkout({lineItems}:any){
+	let stripePromise: Promise<Stripe | null> | null = null;
+
+	const getStripe = () => {
+		if(!stripePromise) {
+			stripePromise = loadStripe(process.env.NEXT_PUBLIC_API_KEY)
+		}
+		return stripePromise
+	}
+
+	const stripe = await getStripe()
+
+    if(stripe){
+
+    
+
+	await stripe.redirectToCheckout({
+		mode: 'payment',
+		lineItems,
+		successUrl: `${window.location.origin}?session_id={CHECKOUT_SESSION_ID}`,
+		cancelUrl: window.location.origin
+	})
+}
+}
